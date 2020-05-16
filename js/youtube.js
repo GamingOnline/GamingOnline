22caa4bfd685755c5b38a48390b0cf44c7b3fef5
GamingOnline <egamingonline545@gmail.com>
2020-05-16 06:48:13 -0600

    First push

---
+++ js/youtube.js
@@ -0,0 +1,72 @@
+var tag = document.createElement('script');
+var videoID = "5_hRB0Kj1eo"
+var videoVolume = 5
+var starttime = 23
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady()
+{
+    player = new YT.Player('player', {
+    height: '100%',
+    width: '100%',
+    videoId: '' + videoID,
+    playerVars: {'showinfo': 0, 'frameborder': 0, 'allowfullscreen': '', 'loop': 1, 'autoplay': 1, 'controls': 0, 'disablekb': 1, 'modestbranding': 1, 'autohide': 1, 'playlist': videoID},
+    events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onStateChanged,
+        'onPlaybackQualityChange': onPlaybackQualityChanged
+    }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+    event.target.seekTo(starttime);
+    event.target.setVolume(videoVolume);
+    event.target.setPlaybackQuality("hd1080");
+}
+
+function onStateChanged(event) {
+    switch (event.data) {
+        case 0:
+            event.target.playVideo();
+            event.target.seekTo(starttime);
+            event.target.setVolume(videoVolume);
+            event.target.setPlaybackQuality("hd1080");
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 5:
+            break;
+    }
+}
+
+function onPlaybackQualityChanged(event) {
+    switch (event.data) {
+        case "small":
+            event.target.setPlaybackQuality("hd1080");
+            break;
+        case "medium":
+            event.target.setPlaybackQuality("hd1080");
+            break;
+        case "large":
+            event.target.setPlaybackQuality("hd1080");
+            break;
+        case "hd720":
+            event.target.setPlaybackQuality("hd1080");
+            break;
+        case "hd1080":
+            break;
+        case "highres":
+            event.target.setPlaybackQuality("hd1080");
+            break;
+    }
+}
