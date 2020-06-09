40cf8a306d1f991f3b7b4cda81680ed8e135df99
GamingOnline <egamingonline545@gmail.com>
2020-06-09 13:49:13 -0600

    v1.3.1
     - Edited youtube.js for the landing page.

--- home/js/youtube.js
+++ home/js/youtube.js
@@ -1,92 +1,109 @@
-var tag = document.createElement('script');
-var videoID = "Lx1iH8DgrLE"
-var videoVolume = 5
-var starttime = 72 // 101.7
-var endtime = 175 //165
-var loopcount = 1;
+var deviceAgent = navigator.userAgent.toLowerCase();
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var isTouchDevice = Modernizr.touch || 
+(deviceAgent.match(/(iphone|ipod|ipad)/) ||
+deviceAgent.match(/(android)/)  || 
+deviceAgent.match(/(iemobile)/) || 
+deviceAgent.match(/iphone/i) || 
+deviceAgent.match(/ipad/i) || 
+deviceAgent.match(/ipod/i) || 
+deviceAgent.match(/blackberry/i) || 
+deviceAgent.match(/bada/i));
 
-var player;
-function onYouTubeIframeAPIReady()
-{
-    player = new YT.Player('player', {
-    height: '100%',
-    width: '100%',
-    videoId: '' + videoID,
-    playerVars: {'showinfo': 0},
-    events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onStateChanged,
-        'onPlaybackQualityChange': onPlaybackQualityChanged
-    }
-    });
-}
+if (isTouchDevice) {
+    console.debug("Mobile")
+} else {
+    console.debug("Desktop")
+    var tag = document.createElement('script');
+    var videoID = "Lx1iH8DgrLE"
+    var videoVolume = 5
+    var starttime = 72 // 101.7
+    var endtime = 175 //165
+    var loopcount = 1;
 
-function onPlayerReady(event) {
-    event.target.playVideo();
-    event.target.seekTo(starttime);
-    event.target.setPlaybackQuality("hd1080");
-    onEndLoop(event);
-}
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-function onEndLoop(event) {    
-    event.target.setVolume(videoVolume);
-    setTimeout(function() {
-        currentTime = event.target.getCurrentTime()
-        //console.log(currentTime); 
-        if (currentTime >= endtime) {
-            event.target.playVideo();
-            event.target.seekTo(starttime);
-            event.target.setVolume(videoVolume);
+    var player;
+    function onYouTubeIframeAPIReady()
+    {
+        player = new YT.Player('player', {
+        height: '100%',
+        width: '100%',
+        videoId: '' + videoID,
+        playerVars: {'showinfo': 0},
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onStateChanged,
+            'onPlaybackQualityChange': onPlaybackQualityChanged
         }
-        loopcount++;
-        if (loopcount < 10) {
-            onEndLoop(event);
-            loopcount = 1
-        }
-    }, 1000)
-}
-
-function onStateChanged(event) {
-    switch (event.data) {
-        case 0:
-            event.target.playVideo();
-            event.target.seekTo(starttime);
-            event.target.setVolume(videoVolume);
-            event.target.setPlaybackQuality("hd1080");
-            break;
-        case 1:
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 5:
-            break;
+        });
     }
-}
 
-function onPlaybackQualityChanged(event) {
-    switch (event.data) {
-        case "small":
-            event.target.setPlaybackQuality("hd1080");
-            break;
-        case "medium":
-            event.target.setPlaybackQuality("hd1080");
-            break;
-        case "large":
-            event.target.setPlaybackQuality("hd1080");
-            break;
-        case "hd720":
-            event.target.setPlaybackQuality("hd1080");
-            break;
-        case "hd1080":
-            break;
-        case "highres":
-            event.target.setPlaybackQuality("hd1080");
-            break;
+    function onPlayerReady(event) {
+        event.target.playVideo();
+        event.target.seekTo(starttime);
+        event.target.setPlaybackQuality("hd1080");
+        onEndLoop(event);
+    }
+
+    function onEndLoop(event) {    
+        event.target.setVolume(videoVolume);
+        setTimeout(function() {
+            currentTime = event.target.getCurrentTime()
+            //console.log(currentTime); 
+            if (currentTime >= endtime) {
+                event.target.playVideo();
+                event.target.seekTo(starttime);
+                event.target.setVolume(videoVolume);
+            }
+            loopcount++;
+            if (loopcount < 10) {
+                onEndLoop(event);
+                loopcount = 1
+            }
+        }, 1000)
+    }
+
+    function onStateChanged(event) {
+        switch (event.data) {
+            case 0:
+                event.target.playVideo();
+                event.target.seekTo(starttime);
+                event.target.setVolume(videoVolume);
+                event.target.setPlaybackQuality("hd1080");
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 5:
+                break;
+        }
+    }
+
+    function onPlaybackQualityChanged(event) {
+        switch (event.data) {
+            case "small":
+                event.target.setPlaybackQuality("hd1080");
+                break;
+            case "medium":
+                event.target.setPlaybackQuality("hd1080");
+                break;
+            case "large":
+                event.target.setPlaybackQuality("hd1080");
+                break;
+            case "hd720":
+                event.target.setPlaybackQuality("hd1080");
+                break;
+            case "hd1080":
+                break;
+            case "highres":
+                event.target.setPlaybackQuality("hd1080");
+                break;
+        }
     }
 }
